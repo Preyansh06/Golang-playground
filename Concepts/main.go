2950d65ecbea1e7d9895fe5a46834b6e0f710e7c
Preyansh Khanna <preyanshkhanna1199@gmail.com>
2025-03-18 18:04:14 +0530

    Playing with var and :=

--- Concepts/main.go
+++ Concepts/main.go
@@ -1,11 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// by default false
+var status bool
 
 func main() {
-	var name string = "Preyansh"
+	// Explicite declaration
+	const name string = "Preyansh"
 	var age int = 23
+	status1 := true
 
-	fmt.Println(name)
-	fmt.Println(age)
+	fmt.Println("Name: ", name)
+	fmt.Println("Age: ", age)
+
+	name2 := "Khanna"
+	age2 := 23
+
+	fmt.Println("Name2:", name2)
+	fmt.Println("Age2:", age2)
+	fmt.Println("Status:", status)
+	fmt.Println("Status1:", status1)
 }
