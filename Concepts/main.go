73661378edaf900b04df037295377cef7b2643af
Preyansh Khanna <preyanshkhanna1199@gmail.com>
2025-03-19 23:45:19 +0530

    if else statement in Go

--- Concepts/main.go
+++ Concepts/main.go
@@ -1,25 +1,35 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func main() {
-	var companyName string
-	var role string
-	var salary float64
+func ifElse() {
+	var companyName, companyName2 string
+	var role, role2 string
+	var salary, salary2 float64
 
-	fmt.Println("Enter comapny name:")
+	fmt.Println("Enter comapny name 1:")
 	fmt.Scanln(&companyName)
 
-	fmt.Println("Enter your role:")
+	fmt.Println("Enter your role 1:")
 	fmt.Scanln(&role)
 
-	fmt.Println("Enter your salary:")
+	fmt.Println("Enter your salary 1:")
 	fmt.Scanln(&salary)
 
-	println("Company name:", companyName)
-	println("Role:", role)
-	fmt.Printf("Salary: %.2f\n", salary)
+	fmt.Println("Enter comapny name 2:")
+	fmt.Scanln(&companyName2)
 
+	fmt.Println("Enter your role 2:")
+	fmt.Scanln(&role2)
+
+	fmt.Println("Enter your salary 2:")
+	fmt.Scanln(&salary2)
+
+	if salary > salary2 {
+		fmt.Println("Salary 1 is greater")
+	} else if salary < salary2 {
+		fmt.Println("Salary 2 is greater")
+	} else {
+		fmt.Println("both are equal")
+	}
 }
