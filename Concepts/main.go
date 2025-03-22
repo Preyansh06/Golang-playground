18f280ff513aa33924b14169070039e079e391d2
Preyansh Khanna <preyanshkhanna1199@gmail.com>
2025-03-22 23:45:02 +0530

    Arrays and loops in Go

--- Concepts/main.go
+++ Concepts/main.go
@@ -3,9 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	var nums int = 5
-	for nums > 0 {
-		fmt.Println(nums)
-		nums--
+	// for loop in Go
+	// var nums int = 5
+	// for nums > 0 {
+	// 	fmt.Println(nums)
+	// 	nums--
+	// }
+
+	var arr [3]string
+	for i := 0; i < 3; i++ {
+		fmt.Println("Enter coding language :")
+		fmt.Scanln(&arr[i])
 	}
+
+	for i := 0; i < 3; i++ {
+		fmt.Printf("Coding Languages: %s\n", arr[i])
+	}
+
 }
