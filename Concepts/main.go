916c19d728f87127be3312ff6f68834078b23e8a
Preyansh Khanna <preyanshkhanna1199@gmail.com>
2025-03-23 23:32:03 +0530

    Playing with for loop and if-else

--- Concepts/main.go
+++ Concepts/main.go
@@ -10,14 +10,31 @@ func main() {
 	// 	nums--
 	// }
 
-	var arr [3]string
-	for i := 0; i < 3; i++ {
-		fmt.Println("Enter coding language :")
-		fmt.Scanln(&arr[i])
-	}
+	// var arr [3]string
+	// for i := 0; i < 3; i++ {
+	// 	fmt.Println("Enter coding language :")
+	// 	fmt.Scanln(&arr[i])
+	// }
+
+	// for i := 0; i < 3; i++ {
+	// 	fmt.Printf("Coding Languages: %s\n", arr[i])
+	// }
+
+	var arr [3]int
+	fmt.Println("Enter 3 numbers:")
 
 	for i := 0; i < 3; i++ {
-		fmt.Printf("Coding Languages: %s\n", arr[i])
+		fmt.Scanf("%d\n", &arr[i])
 	}
+
+	maxNum := arr[0]
+
+	for i := 0; i < 3; i++ {
+		if maxNum < arr[i] {
+			maxNum = arr[i]
+		}
+	}
+
+	fmt.Println("The highest number entered is: ", maxNum)
 
 }
